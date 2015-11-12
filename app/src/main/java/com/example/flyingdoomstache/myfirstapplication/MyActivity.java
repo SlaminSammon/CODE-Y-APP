c44eedffd121c277a6c29794e2a6a60bffb59e8e
skyrimjob <sbachlet@gmail.com>
2015-11-12 08:37:35 -0700

    Original
    This is the original commit repository

---
+++ app/src/main/java/com/example/flyingdoomstache/myfirstapplication/MyActivity.java
@@ -0,0 +1,63 @@
+package com.example.flyingdoomstache.myfirstapplication;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.EditText;
+
+public class MyActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_my);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_my, menu);
+        return true;
+    }
+
+    public void DataStructures(View view) {
+        // Do something in response to button
+        Intent intent = new Intent(this, DataStructuresActivity.class);
+        startActivity(intent);
+    }
+
+    public void Algorithms(View view) {
+        // Do something in response to button
+        Intent intent = new Intent(this, AlgorithmsActivity.class);
+        startActivity(intent);
+    }
+
+    public void CommonProblems(View view) {
+        // Do something in response to button
+        Intent intent = new Intent(this, CommonProblemsActivity.class);
+        startActivity(intent);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
