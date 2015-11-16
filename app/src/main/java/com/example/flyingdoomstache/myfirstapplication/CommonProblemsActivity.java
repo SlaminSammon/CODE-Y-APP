721863eeb0a1f2529a3e585afe5b08db865ae18f
SlaminSammon <awwildcat77@gmail.com>
2015-11-16 08:38:56 -0700

    Common Problems Gui 1.0
    Unlike Algo Gui buttons are not clickable as of  yet

--- app/src/main/java/com/example/flyingdoomstache/myfirstapplication/CommonProblemsActivity.java
+++ app/src/main/java/com/example/flyingdoomstache/myfirstapplication/CommonProblemsActivity.java
@@ -6,8 +6,16 @@ import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
-public class CommonProblemsActivity extends AppCompatActivity {
+public class CommonProblemsActivity extends AppCompatActivity implements AdapterView.OnItemSelectedListener{
+
+    private Spinner spinnerInterview;
+    private static final String[]paths = {"Queue using 2 Stacks", "String Palindrome", "Rotated Array", "ParseInt","SquareRoot"};
+    private Spinner spinnerPuzzles;
+    private static final String[]paths2 = {"Towers of Hanoi", "Dining Philosiphers", "Traveling Salesman",};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,15 +24,31 @@ public class CommonProblemsActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+        spinnerInterview = (Spinner)findViewById(R.id.spinner_item);
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(CommonProblemsActivity.this,
+                android.R.layout.simple_spinner_item,paths);
+
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinnerInterview.setAdapter(adapter);
+        spinnerInterview.setOnItemSelectedListener(this);
+
+        spinnerPuzzles = (Spinner)findViewById(R.id.spinner_item_2);
+        ArrayAdapter<String> adapter2 = new ArrayAdapter<String>(CommonProblemsActivity.this,
+                android.R.layout.simple_spinner_item,paths2);
+
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinnerPuzzles.setAdapter(adapter2);
+        spinnerPuzzles.setOnItemSelectedListener(this);
+
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+    }
+
+    public void onItemSelected(AdapterView<?> parent, View v, int position, long id) {
+        // On selecting a spinner item
+        String item = parent.getItemAtPosition(position).toString();
+    }
+    public void onNothingSelected(AdapterView<?> parent) {
+        // TODO Auto-generated method stub
     }
 
 }
