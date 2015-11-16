987ed39f894afd43e6e4e2f4dbe071acfd382829
SlaminSammon <awwildcat77@gmail.com>
2015-11-16 08:39:46 -0700

    DS GUI 1.0
    Same as Common Problems GUI for now

--- app/src/main/java/com/example/flyingdoomstache/myfirstapplication/DataStructuresActivity.java
+++ app/src/main/java/com/example/flyingdoomstache/myfirstapplication/DataStructuresActivity.java
@@ -6,25 +6,56 @@ import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
-public class DataStructuresActivity extends AppCompatActivity {
-
+public class DataStructuresActivity extends AppCompatActivity implements AdapterView.OnItemSelectedListener{
+    private Spinner spinnerLinear;
+    private static final String[]paths = {"Linked List", "Doubly Linked List", "Circularly Linked List", "Skip List"};
+    private Spinner spinnerTrees;
+    private static final String[]paths2 = {"Default Tree", "BST", "Balanced BST", "B Tree", "Red-Black Tree"};
+    private Spinner spinnerHash;
+    private static final String[]paths3 = {"Hash Table", "Hash Tree", "Double Hashing",};
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_data_structures);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+        spinnerLinear = (Spinner)findViewById(R.id.spinner_item);
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(DataStructuresActivity.this,
+                android.R.layout.simple_spinner_item,paths);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinnerLinear.setAdapter(adapter);
+        spinnerLinear.setOnItemSelectedListener(this);
+
+        spinnerTrees = (Spinner)findViewById(R.id.spinner_item_2);
+        ArrayAdapter<String> adapter2 = new ArrayAdapter<String>(DataStructuresActivity.this,
+                android.R.layout.simple_spinner_item,paths2);
+
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinnerTrees.setAdapter(adapter2);
+        spinnerTrees.setOnItemSelectedListener(this);
+
+        spinnerHash = (Spinner)findViewById(R.id.spinner_item_3);
+        ArrayAdapter<String> adapter3 = new ArrayAdapter<String>(DataStructuresActivity.this,
+                android.R.layout.simple_spinner_item,paths3);
+
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinnerHash.setAdapter(adapter3);
+        spinnerHash.setOnItemSelectedListener(this);
+
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+    }
+
+    public void onItemSelected(AdapterView<?> parent, View v, int position, long id) {
+        // On selecting a spinner item
+        String item = parent.getItemAtPosition(position).toString();
+    }
+    public void onNothingSelected(AdapterView<?> parent) {
+        // TODO Auto-generated method stub
     }
 
 }
